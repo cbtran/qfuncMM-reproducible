9bde33ca9bff688f9e867fb42be66a56991e6191
roobnloo <71776622+roobnloo@users.noreply.github.com>
2023-04-21 01:27:24 +0000

    visualize boxplots for full run excludes strong-strong setting

--- bash/reproduce_all.sh
+++ bash/reproduce_all.sh
@@ -10,7 +10,9 @@ for signal in ${signal_list[@]}
 do
   for intra in ${intra_list[@]}
   do
+    echo "starting signal = $signal, intra = $intra"
     Rscript ./R_files/simulation.R $signal $intra $L $sidelength $M $num_sim $K
+    echo "finished signal = $signal, intra = $intra"
   done
 done
 Rscript ./R_files/summary_all.R $M $num_sim
