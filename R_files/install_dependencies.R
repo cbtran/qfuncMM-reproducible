8cd61455628fc50ad0bf72a161fb7872f845b05c
roobnloo <71776622+roobnloo@users.noreply.github.com>
2024-01-02 19:27:15 +0000

    remove old data files and simulation settings

--- R_files/install_dependencies.R
+++
@@ -1,4 +0,0 @@
-install.packages(c("ggplot2", "tidyverse", "readr",
-                   "ggpubr", "corrplot", "latex2exp",
-                   "Matrix", "xtable", "ggridges"),
-                 repos = "http://cran.us.r-project.org")
